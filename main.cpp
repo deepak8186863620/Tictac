933fcb5f18e7a76df39d4c6e0d698e5df7bf0440
Deepak Prajapati <deepakprajapatid021@gmail.com>
2025-04-26 11:30:40 +0530

    code change

--- main.cpp
+++ main.cpp
@@ -1,10 +1,24 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class node{
-    private:
-    int data;
+
+char board[3][3]; // Tic-Tac-Toe board
+char currentPlayer = 'X';
+
+void initializeBoard() {
+    for (int i = 0; i < 3; ++i)
+        for (int j = 0; j < 3; ++j)
+            board[i][j] = '1' + (i * 3 + j); // Fill with 1-9
 }
-int main(){
-    cout<<"ds project"<<endl;
-    cout<<"cbit"<<endl;
+
+void printBoard() {
+    cout << "\n";
+    for (int i = 0; i < 3; ++i) {
+        cout << " ";
+        for (int j = 0; j < 3; ++j) {
+            cout << board[i][j];
+            if (j < 2) cout << " | ";
+        }
+        if (i < 2) cout << "\n-----------\n";
+    }
+    cout << "\n\n";
 }
