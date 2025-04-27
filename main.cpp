1f0035b6f1737051d84fff708d2de629e9196a46
Deepak Prajapati <deepakprajapatid021@gmail.com>
2025-04-27 19:45:07 +0530

    designing the board

--- main.cpp
+++ main.cpp
@@ -22,3 +22,28 @@ void printBoard() {
     }
     cout << "\n\n";
 }
+bool makeMove(int move) {
+    int row = (move - 1) / 3;
+    int col = (move - 1) % 3;
+
+    if (board[row][col] != 'X' && board[row][col] != 'O') {
+        board[row][col] = currentPlayer;
+        return true;
+    }
+    return false;
+}
+
+bool checkWin() {
+    
+    for (int i = 0; i < 3; ++i)
+        if ((board[i][0] == currentPlayer && board[i][1] == currentPlayer && board[i][2] == currentPlayer) ||
+            (board[0][i] == currentPlayer && board[1][i] == currentPlayer && board[2][i] == currentPlayer))
+            return true;
+
+    
+    if ((board[0][0] == currentPlayer && board[1][1] == currentPlayer && board[2][2] == currentPlayer) ||
+        (board[0][2] == currentPlayer && board[1][1] == currentPlayer && board[2][0] == currentPlayer))
+        return true;
+
+    return false;
+}
